1447aec0a38ba0f596cddb73d529c494efe48a27
franklihs <franklihs@hotmail.com>
2021-09-14 19:04:40 -0400

    tempo aumentado pra 100ms

--- fire.js
+++ fire.js
@@ -9,7 +9,7 @@ function start() {
     createFireSource()
     renderFire()
     
-    setInterval(calculateFirePropagation, 1)
+    setInterval(calculateFirePropagation, 100)
 }
 
 function createFireDataStructure() {

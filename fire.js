beb63c7c62299f2a46a78a24580c5e7d81422a0e
franklihs <franklihs@hotmail.com>
2021-09-14 09:48:01 -0400

    reduzido tempo pra 1ms

--- fire.js
+++ fire.js
@@ -9,7 +9,7 @@ function start() {
     createFireSource()
     renderFire()
     
-    setInterval(calculateFirePropagation, 10)
+    setInterval(calculateFirePropagation, 1)
 }
 
 function createFireDataStructure() {

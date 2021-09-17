5d03c869018a70d8f5fd780fa38bc60c89fdb03c
franklihs <franklihs@hotmail.com>
2021-09-17 18:12:56 -0400

    alterado completamente a branch para conter as pastas do doomfire e dos cartoes para conter uma pagina inicial que guie para as outras e mudança nos nomes dos arquivos de cada pagina

---
+++ home.js
@@ -0,0 +1,28 @@
+// https://ideas-lemon.vercel.app/ 
+import {useState} from 'react';
+
+function Home () {
+    return (
+        <div>
+            <h1>Home 1</h1>
+            <Contador />
+            <a href="/DoomFire/doomfire">Acessar página do fogo do Doom</a>
+        </div>
+    )    
+}
+
+function Contador () {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
